1ebb0bf1eb239ac53c8eb7038e2f968de917a7f5
spenc <spenc@LAPTOP-8LL6OQLE>
2017-06-06 11:57:22 -0600

    Add project files.

---
+++ US_Population_App/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace US_Population_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.getDataButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.averageDisplayLabel = new System.Windows.Forms.Label();
+            this.greatestDisplayLabel = new System.Windows.Forms.Label();
+            this.leastDisplayLabel = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(134, 126);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(190, 212);
+            this.listBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "US Population App";
+            // 
+            // getDataButton
+            // 
+            this.getDataButton.Location = new System.Drawing.Point(193, 74);
+            this.getDataButton.Name = "getDataButton";
+            this.getDataButton.Size = new System.Drawing.Size(75, 23);
+            this.getDataButton.TabIndex = 2;
+            this.getDataButton.Text = "Get Data";
+            this.getDataButton.UseVisualStyleBackColor = true;
+            this.getDataButton.Click += new System.EventHandler(this.getDataButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Average annual change in population:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 413);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Year with the greatest increase in population:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 443);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Year with the least increase in population:";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(134, 492);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // averageDisplayLabel
+            // 
+            this.averageDisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.averageDisplayLabel.Location = new System.Drawing.Point(338, 368);
+            this.averageDisplayLabel.Name = "averageDisplayLabel";
+            this.averageDisplayLabel.Size = new System.Drawing.Size(100, 22);
+            this.averageDisplayLabel.TabIndex = 8;
+            this.averageDisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // greatestDisplayLabel
+            // 
+            this.greatestDisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.greatestDisplayLabel.Location = new System.Drawing.Point(338, 404);
+            this.greatestDisplayLabel.Name = "greatestDisplayLabel";
+            this.greatestDisplayLabel.Size = new System.Drawing.Size(100, 22);
+            this.greatestDisplayLabel.TabIndex = 9;
+            this.greatestDisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // leastDisplayLabel
+            // 
+            this.leastDisplayLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.leastDisplayLabel.Location = new System.Drawing.Point(338, 443);
+            this.leastDisplayLabel.Name = "leastDisplayLabel";
+            this.leastDisplayLabel.Size = new System.Drawing.Size(100, 22);
+            this.leastDisplayLabel.TabIndex = 10;
+            this.leastDisplayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFileDialog";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(275, 492);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 550);
+            this.Controls.Add(this.leastDisplayLabel);
+            this.Controls.Add(this.greatestDisplayLabel);
+            this.Controls.Add(this.averageDisplayLabel);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.getDataButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox);
+            this.Name = "Form1";
+            this.Text = "US Population App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button getDataButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label averageDisplayLabel;
+        private System.Windows.Forms.Label greatestDisplayLabel;
+        private System.Windows.Forms.Label leastDisplayLabel;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ US_Population_App/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/*
+ Spencer Henze
+ ITM 225
+ 10/26/16
+ Homework #6
+ */ 
+namespace US_Population_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Begin Methods:
+
+        private double Average(int[] iArray)
+        {
+            int total = 0; // Accumulator, initialized to 0
+            double average; // to hold the average
+
+            // step throught he array, adding each element to the accumulator.
+
+            for (int index = 0; index < iArray.Length; index++)
+            {
+                total += iArray[index];
+            }
+
+            // now calculate the average
+
+            average = (double)total / iArray.Length;
+            return average;
+        } // end Average() method
+
+        private int Greatest(int[] iArray)
+        {
+            int greatest = iArray[0]; //declare a variable to hold the highest value, and initialize it with the first value in the array.
+            int greatestYear = 1950; // declare the variable that will hold the year that corresponds to the highest increase.
+
+            for (int index = 1; index < iArray.Length; index++) // step through the rest of the array, beginning at element 1. When a value greater than highest is found, assign that value to highest.
+            {
+                if (iArray[index] > greatest) //if the value of the index is greater than
+                {
+                    greatest = iArray[index];
+                    greatestYear = 1950 + index;
+                }
+            }
+
+            return greatestYear;
+        } // end Highest() method
+
+        private int Least(int[] iArray)
+        {
+            int least = iArray[1]; //declare a variable to hold the lowest value, and initialize it with the SECOND value in the array (because the first value is 0 which is meaningless to us and nothing will be less than zero).
+            int leastYear = 0; // declare a variable that will hold the year that corresponds to the value of the 'least' variable
+
+            for (int index = 1; index < iArray.Length; index++) // step through the rest of the array, beginning at element 1. When a value greater than highest is found, assign that value to highest.
+            {
+                if (iArray[index] < least && index !=0)
+                {
+                    least = iArray[index];
+                    leastYear = 1950 + index;
+                }
+
+            }
+
+            return leastYear;
+        }// end Lowest() method
+
+        private void getDataButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            averageDisplayLabel.Text = "";
+            greatestDisplayLabel.Text = "";
+            leastDisplayLabel.Text = "";
+
+            try
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    //declare the arrays (inside of click button because it will change every time the data is fetched)
+                    //Array for populations:
+                    const int SIZE = 40;
+                    int[] yearPopulations = new int[SIZE];  //declare an array with 40 rows and two columns to store the population values
+
+                    // Array 2 for changes:
+                    const int CSIZE = 40;
+                    int[] changesArray = new int[CSIZE]; // declare the second array that will hold the change values.
+
+                    int index = 0; // loop counter for population
+                    int index2 = 0;
+                    int greatestIncrease;
+                    int leastIncrease;
+                    double averageIncrease;
+                    StreamReader inputFile;
+
+
+
+                    inputFile = File.OpenText(openFile.FileName); // assign the open file to the variable 'inputFile'
+
+            //begin the loop for adding the population values to the yearPopulations array
+                    while (index < yearPopulations.Length && !inputFile.EndOfStream)
+                    {
+                        yearPopulations[index] = int.Parse(inputFile.ReadLine()) * 1000;
+                        index++;
+                    }
+
+                    inputFile.Close(); // close the file
+
+           //add difference values to the difference array
+                    ;
+                    while (index2 < changesArray.Length)
+                    {
+                        if (index2 > 0)
+                        {
+                            changesArray[index2] = yearPopulations[index2] - yearPopulations[index2 - 1];
+                            index2++;
+                        }
+                        else
+                        {
+                            changesArray[index2] = 0;
+                            index2++;
+                        }
+
+                    }
+
+               //Display the array info in the ListBox:
+
+                    listBox.Items.Add("Year  " + "Increase\n"); // this is the header for the columns in the listbox.
+                    //now setp through and add items from the array to the listBox:
+                    for (int index3 = 0; index3 < changesArray.Length; index3++)
+                    {
+                        listBox.Items.Add(index3 + 1951 + "  " + changesArray[index3]); // this adds the year (index3 + 1951), two spaces, and the changes array values.
+                    } // end for loop
+
+
+              //Now use methods to do calculations on data 
+
+                    greatestIncrease = Greatest(changesArray);
+                    leastIncrease = Least(changesArray);
+                    averageIncrease = Average(changesArray);
+
+             //display the results in the labels.
+                    greatestDisplayLabel.Text = greatestIncrease.ToString();
+                    leastDisplayLabel.Text = leastIncrease.ToString();
+                    averageDisplayLabel.Text = averageIncrease.ToString("N1");
+
+                } // end if block
+            } //end try block
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        } // end getDataButton_Click()
+
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        } // end exitButton click event
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            averageDisplayLabel.Text = "";
+            greatestDisplayLabel.Text = "";
+            leastDisplayLabel.Text = "";
+        } // end clearButton click event
+    }
+}
